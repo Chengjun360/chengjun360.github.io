130661003edbb5722be1fe64e147afd8a2b1e7cd
Chengjun360 <tcj1989@gmail.com>
2021-02-07 17:54:31 +0100

    init

---
+++ precache-manifest.83cdb1d58a77dc680d6561ff2dae7c96.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d16e53f641083566f2e",
+    "url": "/css/app.17d0086d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e83ede0ec1412769cca8d72159f025e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "305cf83fefb2a576acff",
+    "url": "/js/about.bc120a5c.js"
+  },
+  {
+    "revision": "8d16e53f641083566f2e",
+    "url": "/js/app.aaa10f40.js"
+  },
+  {
+    "revision": "dcdf5be343b133eaabed",
+    "url": "/js/chunk-vendors.78b60929.js"
+  },
+  {
+    "revision": "4dd2751dfa44c648270334e74aa5aa10",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
